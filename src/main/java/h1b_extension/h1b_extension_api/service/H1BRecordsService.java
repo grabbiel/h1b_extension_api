d2182346d99d2826e8506f51463f0432596a6e6c
pakitow <fcruzado@mail.stmarytx.edu>
2023-02-21 19:44:09 -0600

    minimized functionality of records controller + service

--- src/main/java/h1b_extension/h1b_extension_api/service/H1BRecordsService.java
+++ src/main/java/h1b_extension/h1b_extension_api/service/H1BRecordsService.java
@@ -1,25 +1,14 @@
 package h1b_extension.h1b_extension_api.service;
 
 import h1b_extension.h1b_extension_api.bean.CompanyRecord;
-import h1b_extension.h1b_extension_api.bean.JobPosting;
 import h1b_extension.h1b_extension_api.bean.StringMatch;
 import h1b_extension.h1b_extension_api.helper.EncodedString;
 import h1b_extension.h1b_extension_api.helper.StringManipulation;
-import h1b_extension.h1b_extension_api.model.H1BRecord;
-import h1b_extension.h1b_extension_api.model.Opening;
-import h1b_extension.h1b_extension_api.model.Ranking;
 import h1b_extension.h1b_extension_api.repository.H1BRecordsRepository;
-import h1b_extension.h1b_extension_api.repository.OpeningRepository;
-import h1b_extension.h1b_extension_api.repository.RankingRepository;
 import jakarta.transaction.Transactional;
 
-import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 @Service
 @Transactional
@@ -27,19 +16,6 @@ public class H1BRecordsService {
 
     @Autowired
     private H1BRecordsRepository h1brecordsRepository;
-    
-    @Autowired
-    private RankingRepository rankingRepository;
-
-    @Autowired
-    private OpeningRepository openingRepository;
-
-    @Autowired
-    private MatchReviewService matchReviewService;
-
-    public List<H1BRecord> listAllRecords(){
-        return h1brecordsRepository.findAll();
-    }
 
     public CompanyRecord getCompanyRecord(String name){
         EncodedString instance = new EncodedString(name);
@@ -50,29 +26,11 @@ public class H1BRecordsService {
         return h1brecordsRepository.getCompanyCode(name);
     }
 
-    public int getCompanyRanking(int id){
-        Ranking rank = rankingRepository.findById(id);
-        return rank.getRanking();
-    }
-
-    public Opening getCompanyOpenings(int id){
-        return openingRepository.findById(id);
-    }
-
     public StringMatch lookCompanyMatch(String name){
         StringManipulation instance = new StringManipulation(name);
         String literal = iterateCall(instance, 0);
         if(literal==""){return new StringMatch();}
         return new StringMatch(literal);
-    }
-    
-    @Async
-    private CompletableFuture<Integer> findInsertMatchRecords(String string_match, String full_match){
-        Set<String> matches = h1brecordsRepository.getCompaniesLike(string_match);
-        for(String s : matches){
-            matchReviewService.parseSubmission(s, full_match, 1, 0);
-        }
-        return CompletableFuture.completedFuture(1);
     }
 
     private String iterateCall(StringManipulation instance, int cutoff){
@@ -83,8 +41,5 @@ public class H1BRecordsService {
         }else{
             return iterateCall(instance, cutoff+1);
         }   
-    }
-    public JobPosting newPosting(JobPosting posting){
-        return posting;
     }
 }
