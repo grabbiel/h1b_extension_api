d9ad0c6c49940081e9a7f2ce56eaf653eea0efb0
pakitow <fcruzado@mail.stmarytx.edu>
2023-02-21 19:54:44 -0600

    removed non-user-facing repositories

--- src/main/java/h1b_extension/h1b_extension_api/repository/MatchReviewRepository.java
+++ src/main/java/h1b_extension/h1b_extension_api/repository/MatchReviewRepository.java
@@ -1,15 +1,1 @@
-package h1b_extension.h1b_extension_api.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.query.Procedure;
-
-import h1b_extension.h1b_extension_api.model.MatchReview;
-
-public interface MatchReviewRepository extends JpaRepository<MatchReview, Integer>{
-    
-    boolean existsMatchReviewByUscis(String uscis_name);
-
-    boolean existsMatchReviewByAlternatename(String alternate_name);
-
-    @Procedure(procedureName = "INSERT_MATCH_REVIEW_RECORD")
-    void insertNewRecord(String uscis_name, int job_board_code, String match_name, int h1b_records_id, int country_code);
-}
+/* migrated ORM class to a separate microservice */
