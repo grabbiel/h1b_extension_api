7ae9a21a98ed936581867fa4f0d62185e1c2ae02
pakitow <fcruzado@mail.stmarytx.edu>
2023-02-21 20:45:57 -0600

    updated main service class

--- src/main/java/h1b_extension/h1b_extension_api/service/H1BRecordsService.java
+++ src/main/java/h1b_extension/h1b_extension_api/service/H1BRecordsService.java
@@ -8,6 +8,7 @@ import h1b_extension.h1b_extension_api.repository.H1BRecordsRepository;
 import jakarta.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,15 +18,13 @@ public class H1BRecordsService {
     @Autowired
     private H1BRecordsRepository h1brecordsRepository;
 
+    @Cacheable(value = "companyRecordCache")
     public CompanyRecord getCompanyRecord(String name){
         EncodedString instance = new EncodedString(name);
         return h1brecordsRepository.getCompanyRecord(instance.getDecodedString());
     }
 
-    public int getCompanyId(String name){
-        return h1brecordsRepository.getCompanyCode(name);
-    }
-
+    @Cacheable(value = "stringMatchCache")
     public StringMatch lookCompanyMatch(String name){
         StringManipulation instance = new StringManipulation(name);
         String literal = iterateCall(instance, 0);
@@ -41,5 +40,9 @@ public class H1BRecordsService {
         }else{
             return iterateCall(instance, cutoff+1);
         }   
+    }
+
+    public int getCompanyId(String name){
+        return h1brecordsRepository.getCompanyCode(name);
     }
 }
