2afbdfaa8c26d8887e18de9818213cf6415826da
pakitow <fcruzado@mail.stmarytx.edu>
2023-02-24 14:50:59 -0600

    Set up producer class for MatchReviews

---
+++ src/main/java/h1b_extension/h1b_extension_api/components/MatchReviewProducer.java
@@ -0,0 +1,23 @@
+package h1b_extension.h1b_extension_api.components;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import h1b_extension.h1b_extension_api.bean.MatchReview;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class MatchReviewProducer {
+    
+    private final KafkaTemplate<String, MatchReview> template;
+
+    private final String topic = "match_review";
+    
+    public void send(MatchReview review){
+        log.info("Sending <MatchReview> JSON Serializer: {}", review);
+        template.send(topic, review);
+    }
+}
