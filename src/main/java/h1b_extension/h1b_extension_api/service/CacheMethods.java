c91ae13ec43ede283c4e6515b8ae141e3d6eb4ee
pakitow <fcruzado@mail.stmarytx.edu>
2023-02-25 11:39:02 -0600

    deleted some imported packages and changed naming

--- src/main/java/h1b_extension/h1b_extension_api/service/CacheMethods.java
+++ src/main/java/h1b_extension/h1b_extension_api/service/CacheMethods.java
@@ -5,19 +5,19 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import h1b_extension.h1b_extension_api.helper.StringManipulation;
-import h1b_extension.h1b_extension_api.repository.H1BRecordsRepository;
+import h1b_extension.h1b_extension_api.repository.H1BRecordRepository;
 
 @Service
 public class CacheMethods {
 
     @Autowired
-    H1BRecordsRepository h1brecordsRepository;
+    H1BRecordRepository h1brecordRepository;
 
     @Cacheable(value="literal_match", key="#instance.encoded_string")
     public String iterateMatch(StringManipulation instance, int cutoff){
         if(instance.size() == cutoff){return "";}
         String literal = instance.getSplit(cutoff);
-        if(h1brecordsRepository.existsH1BRecordByCompany(literal) || h1brecordsRepository.existsH1BRecordByCompanyStartingWith(literal+" ")){
+        if(h1brecordRepository.existsH1BRecordByCompany(literal) || h1brecordRepository.existsH1BRecordByCompanyStartingWith(literal+" ")){
             return literal;
         }else{
             return iterateMatch(instance, cutoff+1);
@@ -28,7 +28,7 @@ public class CacheMethods {
     public int iterateStatus(StringManipulation instance, int cutoff){
         if(instance.size() == cutoff){return 0;}
         String literal = instance.getSplit(cutoff);
-        if(h1brecordsRepository.existsH1BRecordByCompany(literal) || h1brecordsRepository.existsH1BRecordByCompanyStartingWith(literal+" ")){
+        if(h1brecordRepository.existsH1BRecordByCompany(literal) || h1brecordRepository.existsH1BRecordByCompanyStartingWith(literal+" ")){
             return 1;
         }else{
             return iterateStatus(instance, cutoff+1);
